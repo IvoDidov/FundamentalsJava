0079b6c63c0a353317202664fdd04f802e849342
kubalarin <ivodidovspam@gmail.com>
2021-12-07 11:24:38 +0200

    Change List done; AnonymousThreat generated

---
+++ Lists/AnonymousThreat.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EnterFileNameHere {
+	
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		/* 
+		 * 
+		 * 
+		 * 
+		 * 
+		*/
+		
+	}
+}
--- Lists/ChangeList.java
+++ Lists/ChangeList.java
@@ -10,7 +10,34 @@ public class ChangeList {
 			.map(Integer::parseInt)
 			.collect(Collectors.toList());
 		
+		List<String> command = Arrays.stream(scanner.nextLine().split(" "))
+			.collect(Collectors.toList());
+			
+		while (!(command.get(0)).equals("end")) {			
+			
+			switch(command.get(0)) {
+				case "Delete" : 
+					int numToRemove = Integer.parseInt(command.get(1));
+					List<Integer> removeList = List.of(numToRemove);
+					list.removeAll(removeList);
+				break;
+				
+				case "Insert" : 
+					int numToAdd = Integer.parseInt(command.get(2));
+					list.add((numToAdd), Integer.parseInt(command.get(1)));
+				break;
+				
+			}
+			
+			command = Arrays.stream(scanner.nextLine().split(" "))
+			.collect(Collectors.toList());
+			
+		}
+		
+		for (int num : list) {
+			System.out.print(num + " ");
+		}
+		
 		
 	}
 }
-
