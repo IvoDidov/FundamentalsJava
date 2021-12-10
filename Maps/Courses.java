56ab7c0561612f75f027e8647480a8af4bc0545d
kubalarin <ivodidovspam@gmail.com>
2021-12-10 15:17:44 +0200

    Courses finished with LINKED HashMap; SoftUniExamResults started

--- Maps/Courses.java
+++ Maps/Courses.java
@@ -1,7 +1,45 @@
 package Maps;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Courses {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		Map<String, List<String>> courses = new LinkedHashMap<>();
+
+		while (!input.equals("end")) {
+			String[] command = input.split(" : ");
+
+			List<String> students = new ArrayList<>();
+			String course = command[0];
+			String student = command[1];
+
+
+			if (!courses.containsKey(course)) {
+				courses.put(course, students);
+			}
+				courses.get(course).add(student);
+
+			input = scanner.nextLine();
+		}
+
+/*		courses.forEach((k, v) -> {
+			int members = v.size();
+			System.out.printf("%s: %d%n", k, members);
+			v.forEach(name -> System.out.printf("-- %s%n",name));
+		});*/
+
+		courses.entrySet().stream()
+				.sorted((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
+				.forEach(e -> {
+					int members = e.getValue().size();
+					System.out.println( e.getKey()+ ": " + members);
+					e.getValue()
+							.stream().sorted(String::compareTo)
+							.forEach(name -> System.out.println("-- " +name));
+				});
 
 	}
 }
---
+++ Maps/SoftUniExamResults.java
@@ -0,0 +1,25 @@
+package Maps;
+
+import java.util.*;
+
+public class SoftUniExamResults {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Map<String, Integer> students = new LinkedHashMap<>();
+		Map<String, Integer> languages = new LinkedHashMap<>();
+
+		String input = scanner.nextLine();
+
+		while(!input.equals("exam finished")) {
+			String[] command = input.split("-");
+			
+
+
+
+
+			input = scanner.nextLine();
+		}
+
+	}
+}
